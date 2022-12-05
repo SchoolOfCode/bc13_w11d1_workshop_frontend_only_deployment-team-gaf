d76a4b39330cd8b17dd07631bf81a3707cca5475
Barbara Millon <millon.barbara1@gmail.com>
2022-12-05 15:25:20 +0000

    2cool4School

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          2 COOL 4 SCHOOL
         </a>
       </header>
     </div>
